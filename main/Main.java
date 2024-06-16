9a8fbeae3bb87ebfaef2ee669e6032868c32f036
maiquocbinh13 <binhaxdoc3@gmail.com>
2024-06-17 00:09:15 +0700

    Main class

---
+++ main/Main.java
@@ -0,0 +1,10 @@
+package main;
+
+import gui.StartWindow;
+
+public class Main {
+    public static void main(String[] args) {
+        StartWindow startWindow = new StartWindow();
+        startWindow.setVisible(true);
+    }
+}
